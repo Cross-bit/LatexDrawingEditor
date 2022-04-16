8971b4c3c1f61e82ccc225bbbc8a25bedcaac559
Cross-bit <ondra.kryz@gmail.com>
2022-04-16 21:47:46 +0200

    added preparation of window viewModel

--- LatexDrawingEditor/ViewModels/MainWindowViewModel.cs
+++ LatexDrawingEditor/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,28 @@
-using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input;
+using Dock.Model.Controls;
+using Dock.Model.Core;
+using ReactiveUI;
 
 namespace LatexDrawingEditor.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase 
+    public class MainWindowViewModel : ReactiveObject
     {
+        private readonly IFactory? _factory;
+        private IRootDock? _layout;
+
+        public IRootDock? Layout {
+            get => _layout;
+            set => this.RaiseAndSetIfChanged(ref _layout, value);
+        }
+
+        public MainWindowViewModel() {
+            
+        }
     }
 }
