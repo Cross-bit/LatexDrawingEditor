ce4a5d6fc80f2c031128fc33e1c97ba648d782ff
Cross-bit <ondra.kryz@gmail.com>
2022-04-16 15:39:40 +0200

    removed default greetings message

--- LatexDrawingEditor/ViewModels/MainWindowViewModel.cs
+++ LatexDrawingEditor/ViewModels/MainWindowViewModel.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace LatexDrawingEditor.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase 
     {
-        public string Greeting => "Welcome to Avalonia!";
+
+
     }
 }
