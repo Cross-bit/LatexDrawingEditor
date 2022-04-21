c2e9c62da5fab1662c58a0cbf551e81b64fa0354
Cross-bit <ondra.kryz@gmail.com>
2022-04-21 23:58:29 +0200

    Add base of implementation (root dockable) of window factory

--- LatexDrawingEditor/ViewModels/EditorWindowFactory.cs
+++ LatexDrawingEditor/ViewModels/EditorWindowFactory.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Dock.Avalonia.Controls;
+using Dock.Model.Controls;
+using Dock.Model.Core;
+using Dock.Model.ReactiveUI;
+using Dock.Model.ReactiveUI.Controls;
+
 namespace LatexDrawingEditor.ViewModels
 {
-    internal class EditorWindowFactory
+    public class EditorWindowFactory : Dock.Model.ReactiveUI.Factory
     {
+        private IRootDock? _rootDock;
+        private IDocumentDock? _documentDock;
+
+        public override IRootDock CreateLayout()
+        {
+
+            // the main root dock 
+            var rootDock = CreateRootDock();
+
+
+
+            var windowLayout = CreateRootDock();
+            windowLayout.Title = "Default";
+
+
+
+            rootDock.IsCollapsable = true;
+            rootDock.ActiveDockable = windowLayout;
+            rootDock.DefaultDockable = windowLayout;
+            rootDock.VisibleDockables = CreateList<IDockable>(windowLayout);
+
+            return rootDock;
+        }
+
+
     }
 }
